f87bf6443ebfc25bd2f9bac13315305d6f22f670
Jennifer Shi <shiyating1998@gmail.com>
2025-06-06 11:09:33 +0800

    robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://ai-faq-demo.vercel.app',
+    generateRobotsTxt: true,
+    sitemapSize: 7000,
+    exclude: [], // 如不想让某些页面出现在 sitemap，可以在这里列出
+  };
+  
